2226447149e528f93f0d3230290b1bd6cd881adc
alanaharo <alan.haro@imail.org>
2025-05-09 12:25:04 -0600

    test

--- errors.ps1
+++ errors.ps1
@@ -1,21 +1,10 @@
-Number of Policy Assignment files = 18
-Write-Error: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Helpers/Build-AssignmentDefinitionEntry.ps1:29:25
+Invoke-AzRestMethod: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Helpers/Search-AzGraphAllItems.ps1:97:25
 Line |
-  29 |              $policyId = Confirm-PolicyDefinitionUsedExists `
-     |                          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     |     Policy name 'allowed-az-external-services' not found in custom or built-in Policies.
-InvalidOperation: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Helpers/Build-AssignmentDefinitionEntry.ps1:82:13
+  97 |              $response = Invoke-AzRestMethod -Method POST `
+     |                          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     | Error while copying content to a stream.
+Write-Error: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Helpers/Get-AzPolicyOrSetDefinitions.ps1:39:24
 Line |
-  82 |              $normalizedEntry.displayName = $DefinitionEntry.displayNa …
-     |              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     | The property 'displayName' cannot be found on this object. Verify that the property exists and can be set.
-InvalidOperation: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Helpers/Build-AssignmentDefinitionEntry.ps1:116:13
-Line |
- 116 |              $normalizedEntry.assignment = @{
-     |              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     | The property 'assignment' cannot be found on this object. Verify that the property exists and can be set.
-Write-Error: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Deploy/Build-DeploymentPlans.ps1:334:9
-Line |
- 334 |          Build-AssignmentPlan `
-     |          ~~~~~~~~~~~~~~~~~~~~~~
-     | Assignment definitions content errors
+  39 |  … Resources = Search-AzGraphAllItems -Query $query -ProgressItemName $p …
+     |                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     | Search-AzGraph REST error for ''  -- 
