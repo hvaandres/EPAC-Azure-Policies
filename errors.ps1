2ff4afcb50a5e1781c4ca3f0e8bf6ba51fd82267
alanaharo <alan.haro@imail.org>
2025-05-09 11:45:43 -0600

    test

--- errors.ps1
+++ errors.ps1
@@ -1,8 +1,19 @@
-Write-Error: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Helpers/Build-AssignmentPlan.ps1:90:40
+Number of Policy Assignment files = 18
+Write-Error: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Helpers/Build-AssignmentDefinitionEntry.ps1:29:25
 Line |
-  90 |  …       $hasErrors, $assignmentsList = Build-AssignmentDefinitionNode `
-     |                                         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     |     Missing nodeName at child of ///Unknown//
+  29 |              $policyId = Confirm-PolicyDefinitionUsedExists `
+     |                          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     |     Policy name 'allowed-az-external-services' not found in custom or built-in Policies.
+InvalidOperation: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Helpers/Build-AssignmentDefinitionEntry.ps1:82:13
+Line |
+  82 |              $normalizedEntry.displayName = $DefinitionEntry.displayNa …
+     |              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     | The property 'displayName' cannot be found on this object. Verify that the property exists and can be set.
+InvalidOperation: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Helpers/Build-AssignmentDefinitionEntry.ps1:116:13
+Line |
+ 116 |              $normalizedEntry.assignment = @{
+     |              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     | The property 'assignment' cannot be found on this object. Verify that the property exists and can be set.
 Write-Error: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Deploy/Build-DeploymentPlans.ps1:334:9
 Line |
  334 |          Build-AssignmentPlan `
