5fe79ed26528bf7f035059301b5bd23ab10abcef
alanaharo <alan.haro@imail.org>
2025-05-09 10:53:58 -0600

    test

---
+++ errors.ps1
@@ -0,0 +1,10 @@
+Write-Error: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Helpers/Build-AssignmentPlan.ps1:90:40
+Line |
+  90 |  …       $hasErrors, $assignmentsList = Build-AssignmentDefinitionNode `
+     |                                         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     |     Missing nodeName at child of ///Unknown//
+Write-Error: /Users/alanharo/Documents/git/enterprise-azure-policy-as-code/Scripts/Deploy/Build-DeploymentPlans.ps1:334:9
+Line |
+ 334 |          Build-AssignmentPlan `
+     |          ~~~~~~~~~~~~~~~~~~~~~~
+     | Assignment definitions content errors
